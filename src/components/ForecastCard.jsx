da5c256961eeab782b82e978da8a1a1a2d602344
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-27 20:09:54 +0200

    defines elements of the forecastCard

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 
 function ForecastCard(props) {
     const { locationWeather } = props;
-    console.log(locationWeather)
+    console.log(typeof locationWeather.main.temp_min)
+
+    const mathRoundTemperature = (temperature) => {
+        console.log(temperature)
+        return Math.round(temperature);
+    }
 
     return (
         <div>
+            <h1>Weather in {locationWeather.name}</h1>
             <div>
-                <h1>Weather in {locationWeather.name}</h1>
-                <h2>General overview</h2>
-                <p>Weather today will be mostly {locationWeather.weather[0].main}</p>
-
-                <h2>Temperature</h2>
-                <p>Average temperature {locationWeather.main.temp} Cº</p>
-                <p>Min {locationWeather.main.temp_min} Cº</p>
-                <p>Max {locationWeather.main.temp_max} Cº</p>
+                <p><strong>{locationWeather.weather[0].main}</strong></p>
+                <div className="flex items-center">
+                    <p className="font-bold text-5xl">{mathRoundTemperature(locationWeather.main.temp)}°</p>
+                    <img src={`http://openweathermap.org/img/wn/${locationWeather.weather[0].icon}@2x.png`}  alt={`weather in ${locationWeather.name}`}/>
+                </div>
             </div>
             <div>
-                <img src={`http://openweathermap.org/img/wn/${locationWeather.weather[0].icon}@2x.png`}  alt={`weather in ${locationWeather.name}`}/>
+                <p>Min {mathRoundTemperature(locationWeather.main.temp_min)}° / Max {mathRoundTemperature(locationWeather.main.temp_max)}°</p>
             </div>
-            
         </div>
     )
 }
