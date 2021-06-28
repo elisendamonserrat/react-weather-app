07c47c8f5fd133b08300d5ba2c9f0ab534503a67
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-28 18:00:56 +0200

    adds input component with API calls

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,11 +9,15 @@ export class SearchBar extends Component {
     }
 
     handleInput = (e) => {
-        console.log(e.target.value)
         const newLocation = e.target.value;
         this.setState({
             location: newLocation,
         })
+    }
+
+    handleNewLocation = () => {
+        const { location } = this.state;
+        this.props.newLocation(location);
     }
 
     render() {
@@ -26,7 +30,11 @@ export class SearchBar extends Component {
                     onChange={this.handleInput}
                     className="py-2 pl-4 w-96 text-lg w-10/12 my-4 border rounded-lg"
                 />  
-                <button type="submit" className=" px-4 py-2 my-2 border rounded-lg bg-yellow-300 rounded-lg font-bold uppercase">
+                <button 
+                    type="submit" 
+                    className="px-4 py-2 my-2 border rounded-lg bg-yellow-300 rounded-lg font-bold uppercase"
+                    onClick={this.handleNewLocation}
+                >
                     Check the Weather
                 </button>
             </>
--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -28,12 +28,26 @@ export class HomePage extends Component {
             .catch(error => console.log(error))
     }
 
+    newSearch = (newLocation) => {
+        const apiKey = process.env.REACT_APP_API_KEY;
+
+        axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${newLocation}&units=metric&appid=${apiKey}`)
+        .then((response) => {
+            this.setState({
+                locationWeather: response.data,
+                status: 'loaded'
+            })
+        })
+        .catch(error => console.log(error))
+    }
+
     render() {
         const { status, location, locationWeather } = this.state;
         return (
             <main className="w-11/12 max-w-screen-sm mx-auto text-center my-8 flex flex-col items-center">
                 This is the home page - Here goes the input componenet
-                <SearchBar />
+                <SearchBar newLocation={this.newSearch}/>
 
                 { status === 'loading' && <p>Loading weather forecast for {location}</p>}
                 { status === 'loaded' && < ForecastCard locationWeather={locationWeather}/>}
