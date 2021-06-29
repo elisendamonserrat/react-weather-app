28c1f0c0355b5a870445365e93f1add9370ba864
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-29 15:30:41 +0200

    creates favourites page view

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav className="w-11/12 max-w-screen-md mx-auto text-center my-4 flex items-center">
-            <img className="w-10 h-10 mr-4 inline-block" src="/img/weather-app.png" alt="Weather app icon"/>
-            <p className="font-bold text-yellow-500 text-xl">Weather App</p>
+        <nav className="w-11/12 max-w-screen-md mx-auto text-center my-4 flex items-center justify-between">
+            <div className="flex items-center">
+                <img className="w-10 h-10 mr-4 inline-block" src="/img/weather-app.png" alt="Weather app icon"/>
+                <p className="font-bold text-yellow-500 text-xl">Weather App</p>
+            </div>
+            <Link to='/favourites'><button className="border px-2 py-2 bg-gray-100 rounded-lg text-sm uppercase inline-block">Favourites</button></Link>
         </nav>
     )
 }
--- src/views/FavouritesPage.jsx
+++ src/views/FavouritesPage.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import Navbar from '../components/Navbar';
 
 export class FavouritesPage extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+                <Navbar />
+                <main className="w-11/12 max-w-screen-sm mx-auto text-center my-4 flex flex-col items-center">
+                    <h1>This is your favourites page</h1>
+                    
+                </main>
+            </>
         )
     }
 }
