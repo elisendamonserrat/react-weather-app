4f88e9b059f5016fbb9279248617555645d00aa0
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-29 15:44:29 +0200

    adds favourites button, WIP to display data on the page

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
 
-function ForecastCard(props) {
-    const { locationWeather } = props;
+import React, { Component } from 'react'
 
-    const mathRoundTemperature = (temperature) => {
+export class ForecastCard extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            favourites: [],
+            
+        }
+    }
+    
+    mathRoundTemperature = (temperature) => {
         return Math.round(temperature);
     }
     
-    const currentDate = () => {
+    currentDate = () => {
         const month = [];
         month[0] = "January";
         month[1] = "February";
@@ -27,23 +35,42 @@ function ForecastCard(props) {
         return date
     }
 
+    addToFavourites = () => {
+        const favouritesCopy = [...this.state.favourites];
+        const { locationWeather } = this.props;
 
-    return (
-        <div className="border my-8 flex flex-col items-center py-4 rounded-md w-11/12 max-w-sm">
-            <button className="self-end mx-8">🤍</button>
+        favouritesCopy.push(locationWeather);
+
+        this.setState({
+            favourites: favouritesCopy,
+        })
+
+    }
+
+
+    render() {
+        const { locationWeather } = this.props;
+        console.log(this.state.favourites)
+
+        return (
+            <div className="border my-8 flex flex-col items-center py-4 rounded-md w-11/12 max-w-sm">
+            <button className="self-end mx-8" onClick={this.addToFavourites}>🤍</button>
             <h1 className="text-3xl">{locationWeather.name}</h1>
-            <p className="text-base text-gray-400 font-semibold">{currentDate()}</p>
+            <p className="text-base text-gray-400 font-semibold">{this.currentDate()}</p>
             <p className="text-gray-500 font-semibold text-xl mt-6">{locationWeather.weather[0].main}, {locationWeather.weather[0].description}</p>
             <div className="flex flex-col items-center">
                     <img src={`http://openweathermap.org/img/wn/${locationWeather.weather[0].icon}@4x.png`}  alt={`weather in ${locationWeather.name}`}/>
             </div>
             <div className="border p-4 rounded-md flex items-center justify-between w-11/12">
-                <p className="text-lg text-gray-400 font-semibold">Min {mathRoundTemperature(locationWeather.main.temp_min)}°</p>
-                <p className="font-bold text-5xl">{mathRoundTemperature(locationWeather.main.temp)}°</p>
-                <p className="text-lg text-gray-400 font-semibold">Max {mathRoundTemperature(locationWeather.main.temp_max)}°</p>
+                <p className="text-lg text-gray-400 font-semibold">Min {this.mathRoundTemperature(locationWeather.main.temp_min)}°</p>
+                <p className="font-bold text-5xl">{this.mathRoundTemperature(locationWeather.main.temp)}°</p>
+                <p className="text-lg text-gray-400 font-semibold">Max {this.mathRoundTemperature(locationWeather.main.temp_max)}°</p>
             </div>
         </div>
-    )
+
+        )
+        
+    }
 }
 
 export default ForecastCard
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className="w-11/12 max-w-screen-md mx-auto text-center my-4 flex items-center justify-between">
-            <div className="flex items-center">
-                <img className="w-10 h-10 mr-4 inline-block" src="/img/weather-app.png" alt="Weather app icon"/>
-                <p className="font-bold text-yellow-500 text-xl">Weather App</p>
-            </div>
+            <Link to='/'>
+                <div className="flex items-center">
+                    <img className="w-10 h-10 mr-4 inline-block" src="/img/weather-app.png" alt="Weather app icon"/>
+                    <p className="font-bold text-yellow-500 text-xl">Weather App</p>
+                </div>
+            </Link>
             <Link to='/favourites'><button className="border px-2 py-2 bg-gray-100 rounded-lg text-sm uppercase inline-block">Favourites</button></Link>
         </nav>
     )
