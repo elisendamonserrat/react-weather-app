631a087233e3eabaf869bac5587de56e053de12d
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-07-03 20:43:16 +0200

    adds a cta and view when there are no fav cities

--- src/views/FavouritesPage.jsx
+++ src/views/FavouritesPage.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 import Navbar from '../components/Navbar';
 import ForecastCard from '../components/ForecastCard';
 
@@ -13,12 +15,27 @@ export class FavouritesPage extends Component {
     }
 
     render() {
-        console.log(this.props.favouritesList)
         const { favouritesList } = this.props;
+        console.log(favouritesList)
         return (
             <>
                 <Navbar />
-                <main className="w-11/12 max-w-screen-sm mx-auto text-center my-4 flex flex-col items-center">
+                <main className="w-11/12 max-w-screen-sm mx-auto text-center flex flex-col items-center">
+                    {!favouritesList.length && 
+                     <div className="w-11/12 max-w-screen-sm mx-auto text-center my-16 flex flex-col items-center">
+                        <p className="font-semibold text-xl mb-8">Seems like you don't have any favourites yet. 
+                        <br></br> 
+                        Select your preferred cities and check them here!</p>
+                        <img  className="w-2/5" src="/img/idea.png" alt="page not found" />
+                        <Link to="/">
+                            <button
+                                className="px-4 py-2 mt-8 border rounded-lg bg-yellow-300 rounded-lg font-bold uppercase"
+                            >
+                              Go to Home Page
+                            </button>
+                        </Link>
+                     </div>
+                    }
                     {favouritesList.map((favourite, index) => {
                         return (
                             <ForecastCard 
